9dd5735699b33accb492b54e52cfbf5b4d272dae
Mark Williams <github@shymedia.com>
2016-12-15 22:16:26 -0800

    Set up linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,15 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+      // Disable `no-console` rule
+      "no-console": 0,
+      // Give a warning if identifiers contain underscores
+      "no-underscore-dangle": 1,
+      // Default to single quotes and raise an error if something
+      // else is used
+      "quotes": [2, "single"],
+      "react/jsx-filename-extension": 0
+    }
 };
