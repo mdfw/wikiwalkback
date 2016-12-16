f4acb36df99a6d5a757cf1ebd644495d5e4eea9f
Mark Williams <github@shymedia.com>
2016-12-15 22:17:10 -0800

    Basic Hello World app running

---
+++ js/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = function App() {
+  return (
+    <div id="mainapp">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-require('babel-polyfill');
+import React from 'react';
+import { render } from 'react-dom';
 
+import App from './components/app';
+
+document.addEventListener('DOMContentLoaded', function() { // eslint-disable-line
+  render(
+    <App />,
+    document.getElementById('app') // eslint-disable-line
+  );
+});
