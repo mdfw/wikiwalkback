61feaf140ac66bc37bfa481a2f9101d45509ec32
Mark Williams <github@shymedia.com>
2017-01-02 22:54:32 -0800

    removed logging

--- src/js/actions/subscribe.js
+++ src/js/actions/subscribe.js
@@ -13,27 +13,20 @@ class Walking {
     const rLen = this.rounds.length;
     for (let i = 0; i < rLen; i++) {
       const round = this.rounds[i];
-      console.log('Determining nextAction for round ' + round.round + '. Full object: ');
-      console.dir(round);
       switch (round.status) {
         case constants.ROUND_STATUS_COMPLETE: {
-          console.log('  Round ' + round.round + ' is complete - no work needed.');
           break;
         }
         case constants.ROUND_STATUS_FETCHING: {
-          console.log('  Round ' + round.round + ' is fetching.');
           if (round.hasFetchedAll()) {
-            console.log('    ...but is finished. Updating round to complete.');
             return actions.updateRound(round.round, constants.ROUND_STATUS_COMPLETE);
           }
           return null;
         }
         case constants.ROUND_STATUS_SETLINKS: {
-          console.log('  Round ' + round.round + ' has set links. Starting fetch.');
           return actions.startFetch(round.round, round.pagesToFetch);
         }
         case constants.ROUND_STATUS_NONE: {
-          console.log('  Round ' + round.round + ' has no status. We need to start it.');
           const previousRound = this.rounds[i - 1];
           if (!previousRound) {
             return actions.walkError('Round ' + round.round + ' could not find a previous round. This should not happen and is not user error.');
@@ -45,7 +38,6 @@ class Walking {
           if (links.length > constants.WALK_WIDTH) {
             return actions.walkError('Too many links returned from round ' + previousRound.round + '. Links: ' + JSON.stringify(links));
           }
-          console.log('    ...with these links: ' + JSON.stringify(links));
           return actions.updateRound(round.round, constants.ROUND_STATUS_SETLINKS, links);
         }
         default: {
@@ -68,8 +60,6 @@ function handleChange() {
   // start with the first search
   if (previousState.walkStatus !== currentState.walkStatus &&
       currentState.walkStatus === constants.WALK_STATUS_START) {
-    console.log('::subscriber: trigger first search with store: ');
-    console.dir(store);
     const linkFromInput = new Wiki.LinkIdentifier(null, currentState.currentInput);
     const pagesToFetch = [linkFromInput];
     store.dispatch(
@@ -92,7 +82,6 @@ function handleChange() {
     }
     return;
   }
-  console.log('::subscriber: return without action');
 }
 
 store.subscribe(handleChange);
