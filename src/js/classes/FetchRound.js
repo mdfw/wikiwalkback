49b18473d70cd189fbebf83d1edc7562e1eee377
Mark Williams <github@shymedia.com>
2017-01-04 21:27:28 -0800

    Clean up and comment

--- src/js/classes/FetchRound.js
+++ src/js/classes/FetchRound.js
@@ -1,10 +1,14 @@
 import constants from '../constants';
 
-/* FetchRound is the holder for all of the wiki page data returned from a fetch.
-   Multiple instances of FetchRound are held in the state.rounds var.
-*/
-
 class FetchRound {
+  /* FetchRound is the holder for all of the wiki page data returned from
+      a specific round of fetching.
+    Multiple instances of FetchRound are held in the state.rounds var.
+    this.round {Number} - the count of the round. Should coorespond to the array loc.
+    this.status {String} - the fetching status. See constants.js for values.
+    this.pagesToFetch {Array of WikiLinkIdentifier} - The pages for this round to fetch.
+    this.pagesFetched {Array of WikiLinkIdentifier} - The set of pages that have been fetched.
+  */
   constructor(round = 0, status = constants.ROUND_STATUS_NONE) {
     this.round = round;
     this.status = status;
@@ -12,38 +16,45 @@ class FetchRound {
     this.pagesFetched = [];  // Filled with WikiPage objects
   }
   hasFetchedAll() {
+    /* Tests to see if all pages that should be fetched have been. */
     if (this.pagesToFetch.length === this.pagesFetched.length) {
       return true;
     }
     return false;
   }
-  getAllLinks() {
-    let theLinks = [];
+  getAllFetchedLinks() {
+    /* Returns all 'linkshere' items for all this.pagesFetched */
+    let links = [];
     this.pagesFetched.forEach(function parsePagesFetched(page) {
-      theLinks = theLinks.concat(page.linkshere);
+      links = links.concat(page.linkshere);
     });
-    return theLinks;
+    return links;
   }
-  getRandomLinks(num = constants.WALK_WIDTH) {
+  getRandomFetchedLinks(num = constants.WALK_WIDTH) {
+    /* Selects a random set of (num) 'linkshere' items from all this.pagesFetched */
     /* Randomization code by gonchuki from http://stackoverflow.com/a/5143910 */
-    const alllinks = this.getAllLinks();
-    if (alllinks.length <= num) {
-      return alllinks;
+    const links = this.getAllFetchedLinks();
+    if (links.length <= num) {
+      return links;
     }
-    const linkscopy = JSON.parse(JSON.stringify(alllinks));
+    const linksCopy = links.slice();
     const randLinks = [];
     for (let i = 0; i < num; i++) {
-      const rnd = Math.floor(Math.random() * linkscopy.length);
-      const link = linkscopy.splice(rnd, 1)[0];
+      const rnd = Math.floor(Math.random() * linksCopy.length);
+      const link = linksCopy.splice(rnd, 1)[0];
       randLinks.push(link);
     }
     return randLinks;
   }
   getFetchedLinks(random = 0) {
+    /* Returns a set of 'linkshere' items from all this.pagesFetched
+     * @param random {number} number of random items to return.
+     * @note If there are less than (random), will return all we have.
+     */
     if (random > 0) {
-      return this.getRandomLinks(random);
+      return this.getRandomFetchedLinks(random);
     }
-    return this.getAllLinks();
+    return this.getAllFetchedLinks();
   }
 }
 
